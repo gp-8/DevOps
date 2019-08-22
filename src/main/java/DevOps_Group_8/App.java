feef228c252c5eb264dfd1a28133811b934d395e
Aye Chan Han <40414317@live.napier.ac.uk>
2019-08-23 03:09:53 +0630

    Travis updated and change the database connection from docker to local.

--- src/main/java/DevOps_Group_8/App.java
+++ src/main/java/DevOps_Group_8/App.java
@@ -15,7 +15,7 @@ public class App {
         App a = new App();
 
         // Connect to database
-        a.connect();
+        a.connect("localhost:33060");
 
         //All the countries in the world organised by largest population to smallest.
         ArrayList<Country> cous = a.getCountry();
@@ -72,7 +72,7 @@ public class App {
     /**
      * Connect to the MySQL database.
      */
-    public void connect()
+    public void connect(String location)
     {
         try
         {
@@ -94,13 +94,13 @@ public class App {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://" + location + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
             catch (SQLException sqle)
             {
-                System.out.println("Failed to connect to database attempt " + i);
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
             }
             catch (InterruptedException ie)
