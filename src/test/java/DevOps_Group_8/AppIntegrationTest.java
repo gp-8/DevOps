74e468a0eab0a896484b42dc3dd70a7da3b4d4ae
MinkhantMiracle <40401632@live.napier.ac.uk>
2019-08-23 10:17:38 +0630

    integration tests update with Tests

--- src/test/java/DevOps_Group_8/AppIntegrationTest.java
+++ src/test/java/DevOps_Group_8/AppIntegrationTest.java
@@ -20,9 +20,39 @@ public class AppIntegrationTest
     }
 
     @Test
+    void printCityTestNull()
+    {
+        app.displayCity(null);
+    }
+    //testing for null value contain in cities record
+    @Test
+    void printCitiesTestContainsNull()
+    {
+        ArrayList<City> cities = new ArrayList<City>();
+        cities.add(null);
+        app.displayCity(cities);
+    }
+
+    //null value testing for displaying countries in a region
+    @Test
     void countries_region_largest_to_smallestTESTNull (){
         ArrayList<Country> countries= new ArrayList<Country>();
         countries.add(null);
         app.displayCountries_region_largest_to_smallest(countries);
+    }
+
+    //null value testing for displaying cities in a continent
+    @Test
+    void displayCityContinentTESTNull(){
+        ArrayList<City> cities = new ArrayList<City>();
+        cities.add(null);
+        app.displayCityContinent(cities);
+    }
+
+    @Test
+    void displayCityRegionTESTNull(){
+        ArrayList<City> cities =new ArrayList<>();
+        cities.add(null);
+        app.displayCityRegion(cities);
     }
 }
